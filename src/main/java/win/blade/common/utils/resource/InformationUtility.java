c5353ddc52b19fdafa9252b612fc3a95af2477a5
LuxeSystem <156671088+buble1234@users.noreply.github.com>
2025-06-18 14:18:59 +0300

    +DiscordRPCModule

---
+++ src/main/java/win/blade/common/utils/resource/InformationUtility.java
@@ -0,0 +1,10 @@
+package win.blade.common.utils.resource;
+
+public class InformationUtility {
+    public static final String CLIENT = "BLADE";
+    public static final String CLIENT_ID = "blade";
+    public static final String VERSION = "1.0";
+    public static final String AUTHOR = "NoCap, Melon, Ieo117";
+    public static final String DISCORD_INVITE = "https://discord.gg/bladedlc";
+    public static final String TELEGRAM_LINK = "https://t.me/bladerecode";
+}
