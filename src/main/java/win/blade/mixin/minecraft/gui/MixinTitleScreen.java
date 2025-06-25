1496745d8ad5e86cb06a03e5c5c90adadd9d1993
LuxeSystem <156671088+buble1234@users.noreply.github.com>
2025-06-25 18:15:08 +0300

    Проверка на паник

--- src/main/java/win/blade/mixin/minecraft/gui/MixinTitleScreen.java
+++ src/main/java/win/blade/mixin/minecraft/gui/MixinTitleScreen.java
@@ -8,12 +8,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import win.blade.common.gui.impl.MainScreen;
 import win.blade.common.utils.minecraft.MinecraftInstance;
+import win.blade.core.Manager;
 
 @Mixin(TitleScreen.class)
 public class MixinTitleScreen implements MinecraftInstance {
 
     @Inject(method = "init", at = @At("RETURN"))
     public void postInitHook(CallbackInfo callbackInfo) {
-        mc.setScreen(new MainScreen());
+        if (!Manager.isPanic()) {
+            mc.setScreen(new MainScreen());
+        }
     }
 }
