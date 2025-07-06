922eb6a6a5f756f934f92c54221c7a11620c2fe1
leo <rbw1nleo@gmail.com>
2025-07-06 22:10:51 +0500

    commit 1488

---
+++ src/main/java/win/blade/common/gui/impl/screen/account/AccountSaver.java
@@ -0,0 +1,64 @@
+package win.blade.common.gui.impl.screen.account;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.commons.compress.compressors.zstandard.ZstdCompressorOutputStream;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Автор Ieo117
+ * Дата создания: 05.07.2025, в 22:21:14
+ */
+@SuppressWarnings("all")
+public class AccountSaver {
+    private static final Gson gson = new Gson();
+    private static final Type token = new TypeToken<List<Account>>(){}.getType();
+    private static final String path = "blade/";
+    private static final String fileName = "accounts.json";
+    public static void save(List<Account> accounts){
+        File file = new File(path + fileName);
+        File path = new File(AccountSaver.path);
+
+        try {
+            file.getParentFile().mkdirs();
+
+            // try-with-resources автоматически закроет writer
+            try (FileWriter writer = new FileWriter(file)) {
+                gson.toJson(accounts, writer);
+            }
+
+            System.out.println("Saved: " + accounts.size());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static List<Account> loadAccounts(){
+        List<Account> accountList = new ArrayList<>();
+
+        File file = new File(path + fileName);
+
+        if(file.exists()) {
+            try (FileReader reader = new FileReader(file)) {
+                List<Account> loadedList = gson.fromJson(reader, token);
+                if(loadedList != null && !loadedList.isEmpty()){
+                    accountList = loadedList;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        accountList.removeIf(Objects::isNull);
+        return accountList;
+    }
+}
--- src/main/java/win/blade/common/gui/impl/screen/account/AccountScreen.java
+++ src/main/java/win/blade/common/gui/impl/screen/account/AccountScreen.java
@@ -2,11 +2,16 @@ package win.blade.common.gui.impl.screen.account;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.session.Session;
 import net.minecraft.client.texture.AbstractTexture;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Uuids;
 import win.blade.common.gui.button.Button;
 import win.blade.common.gui.impl.screen.BaseScreen;
+import win.blade.common.utils.math.MathUtility;
+import win.blade.common.utils.minecraft.MinecraftUtility;
 import win.blade.common.utils.render.builders.Builder;
 import win.blade.common.utils.render.builders.states.QuadColorState;
 import win.blade.common.utils.render.builders.states.QuadRadiusState;
@@ -17,14 +22,34 @@ import win.blade.common.utils.render.renderers.impl.BuiltRectangle;
 import win.blade.common.utils.render.renderers.impl.BuiltTexture;
 
 import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class AccountScreen extends BaseScreen {
+    public final List<Account> accountList = new CopyOnWriteArrayList<>();
+
     private int windowX;
     private int windowY;
+//    public TextFieldWidget input;
+    public Account selected;
 
     public AccountScreen() {
         super(Text.of("Account Management"));
+        var loadedAccounts = AccountSaver.loadAccounts();
+        if(!loadedAccounts.isEmpty()) {
+            accountList.addAll(loadedAccounts);
+            selected = accountList.get(0);
+        }
+
+        selected = new Account(mc.getSession().getUsername(), System.currentTimeMillis());
     }
+
 
     @Override
     protected void init() {
@@ -37,7 +62,13 @@ public class AccountScreen extends BaseScreen {
                 276,
                 22,
                 Text.of("+"),
-                () -> System.out.println("Add button clicked")
+                () -> {
+                    String randomName = UUID.randomUUID().toString().replaceAll("-", "").substring(0, ThreadLocalRandom.current().nextInt(8, 15));
+                    Account account = new Account(randomName, LocalDateTime.now());
+                    accountList.add(account);
+                    changeAccount(account);
+                    AccountSaver.save(accountList);
+                }
         ));
 
         this.addDrawableChild(new Button(
@@ -46,8 +77,16 @@ public class AccountScreen extends BaseScreen {
                 306,
                 35,
                 Text.of("Exit"),
-                () -> this.close()
+                this::close
         ));
+
+    }
+
+    @Override
+    public void close() {
+        super.close();
+
+        AccountSaver.save(accountList);
     }
 
     @Override
@@ -71,26 +110,33 @@ public class AccountScreen extends BaseScreen {
                 .build()
                 .render(windowX + 25, windowY + 17);
 
-        String[] place = {"cutthroatpublicenemy", "NoCap", "fFDSH672"};
 
 
         int entryH = 50;
         int gap = 5;
         int startY = windowY + 40;
 
-        for (int i = 0; i < place.length; i++) {
+        for (int i = 0; i < accountList.size(); i++) {
             int entryY = startY + (i * (entryH + gap));
-
+            var account = accountList.get(i);
+            String accountName = account.getUsername();
+            LocalDateTime dateTime = account.getCreationDateTime();
+            boolean selected = accountName.equals(this.selected.getUsername());
 
             Color d = new Color(20,18,27,255);
 
-            BuiltRectangle rectangle = Builder.rectangle()
+            if(selected){
+//                System.out.println("selec");
+                d = d.brighter().brighter();
+            }
+
+            Builder.rectangle()
                     .size(new SizeState(276, entryH))
                     .color(new QuadColorState(d))
                     .radius(new QuadRadiusState(10))
                     .smoothness(1.0f)
-                    .build();
-            rectangle.render( windowX + 15, entryY);
+                    .build()
+                    .render( windowX + 15, entryY);
 
 
             BuiltBorder border = Builder.border()
@@ -121,7 +167,7 @@ public class AccountScreen extends BaseScreen {
 
             Builder.text()
                     .font(FontType.sf_regular.get())
-                    .text(place[i])
+                    .text(accountName)
                     .color(new Color(255, 255, 255))
                     .size(10)
                     .build()
@@ -130,7 +176,7 @@ public class AccountScreen extends BaseScreen {
 
             Builder.text()
                     .font(FontType.sf_regular.get())
-                    .text(place[i])
+                    .text(accountName)
                     .color(new Color(255, 255, 255))
                     .size(10)
                     .build()
@@ -138,7 +184,7 @@ public class AccountScreen extends BaseScreen {
 
             Builder.text()
                     .font(FontType.sf_regular.get())
-                    .text("01.01.24 / 10:00")
+                    .text(dateTime.format(DateTimeFormatter.ISO_DATE))
                     .color(new Color(150, 150, 150))
                     .size(7)
                     .build()
@@ -165,5 +211,48 @@ public class AccountScreen extends BaseScreen {
 
         }
 
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        int startY = windowY + 40;
+        int entryH = 50;
+        int gap = 5;
+
+
+        for (int i = 0; i < accountList.size(); i++) {
+            int entryY = startY + (i * (entryH + gap));
+            var account = accountList.get(i);
+
+            if(MathUtility.isHovered(mouseX, mouseY, windowX + 15, entryY, 276, entryH)){
+
+                if(MathUtility.isHovered(mouseX, mouseY, windowX + 270, entryY + 15, 8.5f, 8.5f)){
+                    accountList.remove(account);
+                } else {
+                    changeAccount(account);
+                }
+
+                break;
+            }
+
+        }
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+
+    @SuppressWarnings("all")
+    public void changeAccount(Account current) {
+        String name = current.getUsername();
+        if (mc.getSession().getUsername().equalsIgnoreCase(name)) return;
+
+        selected = current;
+        Session newSession = new Session(name, Uuids.getOfflinePlayerUuid(name), "", Optional.empty(), Optional.empty(), Session.AccountType.MOJANG);
+        try {
+            MinecraftUtility.setSession(newSession);
+        } catch (Throwable t){
+            t.printStackTrace();
+        }
+
+        AccountSaver.save(accountList);
     }
 }
