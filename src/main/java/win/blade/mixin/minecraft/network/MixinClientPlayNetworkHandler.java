4c6695835cfbcfe93c36568c049b115f9d469256
LuxeSystem <156671088+buble1234@users.noreply.github.com>
2025-07-18 20:04:07 +0300

    добил

--- src/main/java/win/blade/mixin/minecraft/network/MixinClientPlayNetworkHandler.java
+++ src/main/java/win/blade/mixin/minecraft/network/MixinClientPlayNetworkHandler.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import win.blade.common.gui.impl.screen.multiplayer.MultiplayerScreen;
 import win.blade.common.utils.system.LayoutUtil;
 import win.blade.core.Manager;
 
