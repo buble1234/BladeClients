47aae1b6e167efdd01d92304f06f429d0a544718
LuxeSystem <156671088+buble1234@users.noreply.github.com>
2025-07-22 15:50:13 +0300

    подсказка

--- src/main/java/win/blade/core/commands/impl/BindCommand.java
+++ src/main/java/win/blade/core/commands/impl/BindCommand.java
@@ -3,14 +3,15 @@ package win.blade.core.commands.impl;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.command.CommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.NotNull;
+import org.lwjgl.glfw.GLFW;
 import win.blade.core.Manager;
 import win.blade.core.commands.Command;
 import win.blade.core.commands.CommandInfo;
+import win.blade.common.utils.keyboard.Keyboard;
 
 import java.util.Locale;
 
@@ -30,27 +31,54 @@ public class BindCommand extends Command {
                     Manager.getModuleManagement().all().forEach(module -> suggestionsBuilder.suggest(module.name()));
                     return suggestionsBuilder.buildFuture();
                 })
-                .then(RequiredArgumentBuilder.<CommandSource, String>argument("key", StringArgumentType.word())
+                .then(LiteralArgumentBuilder.<CommandSource>literal("remove")
                         .executes(context -> {
                             String moduleName = StringArgumentType.getString(context, "module");
-                            String keyName = StringArgumentType.getString(context, "key").toUpperCase();
+                            return Manager.getModuleManagement().find(moduleName)
+                                    .map(module -> {
+                                        module.setKeybind(GLFW.GLFW_KEY_UNKNOWN);
+                                        mc.player.sendMessage(Text.literal("Бинд для модуля ").append(Text.literal(module.name()).formatted(Formatting.AQUA)).append(Text.literal(" был удален.").formatted(Formatting.WHITE)), false);
+                                        return SINGLE_SUCCESS;
+                                    })
+                                    .orElseGet(() -> {
+                                        mc.player.sendMessage(Text.literal("Модуль '").append(Text.literal(moduleName).formatted(Formatting.RED)).append(Text.literal("' не найден.")), false);
+                                        return 0;
+                                    });
+                        }))
+                .then(RequiredArgumentBuilder.<CommandSource, String>argument("key", StringArgumentType.word())
+                        .suggests((context, suggestionsBuilder) -> {
+                            for (Keyboard key : Keyboard.values()) {
+                                suggestionsBuilder.suggest(key.getName());
+                            }
+                            suggestionsBuilder.suggest("NONE");
+                            return suggestionsBuilder.buildFuture();
+                        })
+                        .executes(context -> {
+                            String moduleName = StringArgumentType.getString(context, "module");
+                            String keyName = StringArgumentType.getString(context, "key").toUpperCase(Locale.ROOT);
 
                             return Manager.getModuleManagement().find(moduleName)
                                     .map(module -> {
                                         if (keyName.equalsIgnoreCase("NONE")) {
-                                            module.setKeybind(0);
+                                            module.setKeybind(GLFW.GLFW_KEY_UNKNOWN);
                                             mc.player.sendMessage(Text.literal("Модуль ").append(Text.literal(module.name()).formatted(Formatting.AQUA)).append(Text.literal(" был отвязан.").formatted(Formatting.WHITE)), false);
                                             return SINGLE_SUCCESS;
                                         }
 
-                                        InputUtil.Key key = InputUtil.fromTranslationKey("key.keyboard." + keyName.toLowerCase(Locale.ROOT));
+                                        int keyCode = -1;
+                                        for (Keyboard k : Keyboard.values()) {
+                                            if (k.getName().equalsIgnoreCase(keyName)) {
+                                                keyCode = k.getKey();
+                                                break;
+                                            }
+                                        }
 
-                                        if (key.getCode() == -1) {
-                                            mc.player.sendMessage(Text.literal("Клавиша '").append(Text.literal(keyName).formatted(Formatting.RED)).append(Text.literal("' не найдена.")), false);
+                                        if (keyCode == -1 || keyCode == GLFW.GLFW_KEY_UNKNOWN) {
+                                            mc.player.sendMessage(Text.literal("Клавиша '").append(Text.literal(keyName).formatted(Formatting.RED)).append(Text.literal("' не найдена или неверна.")), false);
                                             return 0;
                                         }
 
-                                        module.setKeybind(key.getCode());
+                                        module.setKeybind(keyCode);
                                         mc.player.sendMessage(Text.literal("Модуль ").append(Text.literal(module.name()).formatted(Formatting.AQUA)).append(Text.literal(" привязан к клавише ")).append(Text.literal(keyName).formatted(Formatting.GOLD)).append(Text.literal(".")), false);
 
                                         return SINGLE_SUCCESS;
@@ -65,11 +93,7 @@ public class BindCommand extends Command {
                     return Manager.getModuleManagement().find(moduleName)
                             .map(module -> {
                                 int keyCode = module.keybind();
-                                String keyName = InputUtil.fromKeyCode(keyCode, -1).getLocalizedText().getString().toUpperCase();
-
-                                if (keyCode == 0) {
-                                    keyName = "NONE";
-                                }
+                                String keyName = Keyboard.getKeyName(keyCode);
 
                                 mc.player.sendMessage(Text.literal("Модуль ").append(Text.literal(module.name()).formatted(Formatting.AQUA)).append(Text.literal(" привязан к клавише ")).append(Text.literal(keyName).formatted(Formatting.GOLD)).append(Text.literal(".")), false);
 
