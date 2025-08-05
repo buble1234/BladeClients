756bb556a1720f4c1485854a765c61ace75b8405
LuxeSystem <156671088+buble1234@users.noreply.github.com>
2025-08-05 17:26:09 +0300

    труешка

--- src/main/java/win/blade/core/module/storage/render/FogBlur.java
+++ src/main/java/win/blade/core/module/storage/render/FogBlur.java
@@ -12,22 +12,21 @@ import win.blade.core.module.api.ModuleInfo;
 @ModuleInfo(name = "FogBlur", category = Category.RENDER, desc = "Добавляет эффект размытого тумана на расстоянии.")
 public class FogBlur extends Module {
 
-    private final ValueSetting fogStrength = new ValueSetting("Сила размытия", "Сила размытия для эффекта тумана.").setValue(6f).range(1f, 20f);
-    private final ValueSetting fogDistance = new ValueSetting("Дистанция тумана", "На какой дистанции появляется эффект тумана.").setValue(50f).range(0f, 100f);
-    private final BooleanSetting linearSampling = new BooleanSetting("Линейная выборка", "Включает линейную выборку для Гауссова размытия.").setValue(true);
-    private final BooleanSetting rainbow = new BooleanSetting("Rainbow", "Включает радужный эффект для тумана.").setValue(false);
+    private final ValueSetting fogStrength = new ValueSetting("Сила размытия", "Сила размытия для эффекта тумана").setValue(6f).range(1f, 20f);
+    private final ValueSetting fogDistance = new ValueSetting("Дистанция тумана", "На какой дистанции появляется эффект тумана").setValue(50f).range(0f, 100f);
+    private final BooleanSetting rainbow = new BooleanSetting("Rainbow", "Включает радужный эффект для тумана").setValue(false);
 
-    private final ValueSetting rainbowStrength = new ValueSetting("Сила радуги", "Интенсивность радужного эффекта.").setValue(0.5f).range(0f, 1f);
+    private final ValueSetting rainbowStrength = new ValueSetting("Сила радуги", "Интенсивность радужного эффекта").setValue(0.5f).range(0f, 1f);
 
 
     public FogBlur() {
-        addSettings(fogStrength, fogDistance, linearSampling, rainbow, rainbowStrength);
+        addSettings(fogStrength, fogDistance, rainbow, rainbowStrength);
     }
 
     @EventHandler
     public void onRenderWorld(RenderEvents.World event) {
         if (mc.player == null || mc.world == null || mc.gameRenderer.isRenderingPanorama()) return;
 
-        ShaderHelper.applyFogBlur(fogStrength.getValue(), fogDistance.getValue(), linearSampling.getValue(), rainbow.getValue(), true, rainbowStrength.getValue());
+        ShaderHelper.applyFogBlur(fogStrength.getValue(), fogDistance.getValue(), true, rainbow.getValue(), true, rainbowStrength.getValue());
     }
 }
