3fd1c82afd3d46a2ba2bb726622e7362cc5164a1
LuxeSystem <156671088+buble1234@users.noreply.github.com>
2025-09-11 13:49:02 +0300

    fix

--- src/main/java/win/blade/common/utils/render/Stencil.java
+++ src/main/java/win/blade/common/utils/render/Stencil.java
@@ -1,41 +1,44 @@
 package win.blade.common.utils.render;
 
-
 import win.blade.common.utils.minecraft.MinecraftInstance;
 
 import static org.lwjgl.opengl.EXTFramebufferObject.*;
-import static org.lwjgl.opengl.EXTFramebufferObject.GL_RENDERBUFFER_EXT;
 import static org.lwjgl.opengl.EXTPackedDepthStencil.GL_DEPTH_STENCIL_EXT;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
-
-import static org.lwjgl.opengl.GL11.GL_STENCIL_TEST;
 
 /**
- * Автор Ieo117
- * Дата создания: 23.06.2025, в 13:21:34
+ * Исправленная версия Stencil класса
+ * Автор Ieo117 (исправлено)
  */
-
 public class Stencil implements MinecraftInstance {
+
+    private static int originalDepthAttachment = -1;
+    private static boolean stencilInitialized = false;
+    private static int stencilDepthBufferID = -1;
 
     public static void push() {
         var framebuffer = mc.getFramebuffer();
-        if (framebuffer.depthAttachment > -1) {
-            mc.getFramebuffer().beginWrite(false);
 
-            glDeleteRenderbuffersEXT(framebuffer.depthAttachment);
-            final int stencilDepthBufferID = glGenRenderbuffersEXT();
+        if (!stencilInitialized && framebuffer.depthAttachment > -1) {
+            originalDepthAttachment = framebuffer.depthAttachment;
+
+            stencilDepthBufferID = glGenRenderbuffersEXT();
             glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, stencilDepthBufferID);
-            glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_STENCIL_EXT, mc.getWindow().getWidth(), mc.getWindow().getHeight());
-            glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_STENCIL_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, stencilDepthBufferID);
-            glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, stencilDepthBufferID);
-            framebuffer.depthAttachment = -1;
+            glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_STENCIL_EXT,
+                    mc.getWindow().getWidth(), mc.getWindow().getHeight());
+
+            glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_STENCIL_ATTACHMENT_EXT,
+                    GL_RENDERBUFFER_EXT, stencilDepthBufferID);
+            glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT,
+                    GL_RENDERBUFFER_EXT, stencilDepthBufferID);
+
+            stencilInitialized = true;
         }
 
         glStencilMask(0xFF);
         glClear(GL_STENCIL_BUFFER_BIT);
         glEnable(GL_STENCIL_TEST);
-        glStencilFunc(GL_ALWAYS, 1, 1);
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
         glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);
         glDisable(GL_DEPTH_TEST);
         glColorMask(false, false, false, false);
@@ -43,19 +46,34 @@ public class Stencil implements MinecraftInstance {
 
     public static void read(int ref) {
         glColorMask(true, true, true, true);
-        glStencilFunc(GL_EQUAL, ref, 1);
+        glStencilFunc(GL_EQUAL, ref, 0xFF);
         glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
     }
 
     public static void pop() {
         glDisable(GL_STENCIL_TEST);
         glEnable(GL_DEPTH_TEST);
+        glColorMask(true, true, true, true);
+        glStencilMask(0xFF);
+
+        restoreFramebuffer();
     }
 
+    public static void erase(int ref) {
+        glColorMask(true, true, true, true);
+        glStencilFunc(GL_NOTEQUAL, ref, 0xFF);
+        glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+    }
+
+    private static void restoreFramebuffer() {
+    }
+
+    public static void cleanup() {
+        if (stencilDepthBufferID > -1) {
+            glDeleteRenderbuffersEXT(stencilDepthBufferID);
+            stencilDepthBufferID = -1;
+        }
+        stencilInitialized = false;
+        originalDepthAttachment = -1;
+    }
 }
-//
-//    public static void erase(int ref) {
-//        glColorMask(true, true, true, true);
-//        glStencilFunc(GL_NOTEQUAL, ref, 1);
-//        glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
-//    }
