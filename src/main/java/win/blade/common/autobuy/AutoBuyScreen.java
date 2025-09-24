829146191e9c7714ddb91af44be47d352beb3db9
LuxeSystem <156671088+buble1234@users.noreply.github.com>
2025-09-24 20:25:21 +0300

    мелону доделать

---
+++ src/main/java/win/blade/common/autobuy/AutoBuyScreen.java
@@ -0,0 +1,26 @@
+package win.blade.common.autobuy;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+/**
+ * Автор: NoCap
+ * Дата создания: 24.09.2025
+ */
+public class AutoBuyScreen extends Screen {
+
+    public AutoBuyScreen() {
+        super(Text.of("AutoBuyScreen"));
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+    }
+}
