f182274131a62ade10f58805124d04e734266625
LuxeSystem <156671088+buble1234@users.noreply.github.com>
2025-10-01 16:53:05 +0300

    убрал лишнее

--- src/main/java/win/blade/Blade.java
+++ src/main/java/win/blade/Blade.java
@@ -1,30 +1,14 @@
 package win.blade;
 
-import com.mojang.authlib.exceptions.AuthenticationException;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.session.Session;
-import net.minecraft.util.Uuids;
-import win.blade.common.utils.browser.BrowserManager;
-import win.blade.common.utils.minecraft.MinecraftUtility;
 import win.blade.core.Manager;
 
-import java.util.Optional;
-
 public class Blade implements ModInitializer {
-
-
 
     public static Manager manager = new Manager();
 
     @Override
     public void onInitialize() {
         manager.init();
-        try {
-            Session newSession = new Session("MagnatYgla", Uuids.getOfflinePlayerUuid("MagnatYgla"), "", Optional.empty(), Optional.empty(), Session.AccountType.MOJANG);
-            MinecraftUtility.setSession(newSession);
-        } catch (AuthenticationException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
